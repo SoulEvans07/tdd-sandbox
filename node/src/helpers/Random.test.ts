0b88ca0dec6610640d4892ae39df3b8c5f8fbe46
Adam Szi <szi.adam@simonyi.bme.hu>
2022-04-29 17:43:38 +0200

    some more tests

--- node/src/helpers/Random.test.ts
+++ node/src/helpers/Random.test.ts
@@ -1,5 +1,4 @@
 import { InvalidArgumentException, Random } from './Random';
-import 'jest-extended';
 
 describe('Random', () => {
   describe('number', () => {
@@ -7,11 +6,28 @@ describe('Random', () => {
       const value = Random.number();
       expect(value).toBeWithin(0, 1);
     });
+
+    test('negative size results in value between 0 and given negative value', () => {
+      expect(Random.number(-5)).toBeWithin(-5, 0);
+    });
+
+    test('poisitve size results in value between 0 and given positive value', () => {
+      expect(Random.number(5)).toBeWithin(0, 5);
+    });
   });
 
   describe('string', () => {
     it('throws InvalidArgumentException when length < 1', () => {
       expect(() => Random.string(0)).toThrowType(InvalidArgumentException);
+    });
+
+    test('output length is equal to the argument', () => {
+      const length = 5;
+      expect(Random.string(length)).toHaveLength(length);
+    });
+
+    test('only returns lowercase characters [a-z]', () => {
+      expect(Random.string(100)).toMatch(/^[a-z]{100}$/);
     });
   });
 });
