6f99c9209cd7bcf4773484f4cfb537a690ce58bc
Adam Szi <szi.adam@simonyi.bme.hu>
2022-04-29 17:45:58 +0200

    Random is changed to class

--- node/src/helpers/Random.ts
+++ node/src/helpers/Random.ts
@@ -1,15 +1,16 @@
-export const Random = {
-  number(size = 1): number {
+export class Random {
+  static number(size = 1): number {
     return Math.floor(Math.random() * size);
-  },
-  string(length: number): string {
+  }
+
+  static string(length: number): string {
     if (length < 1) throw new InvalidArgumentException('length must be bigger than 0');
     return new Array(length)
       .fill(null)
       .map(() => String.fromCharCode(97 + this.number(26)))
       .join('');
-  },
-} as const;
+  }
+}
 
 export class InvalidArgumentException extends Error {
   name = 'InvalidArgumentException';
