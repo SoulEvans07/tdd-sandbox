f1dc7230fc0222a01164cb2191c1a00f7dec173f
András Balogh <kokx_007@msn.com>
2022-05-11 16:37:28 +0200

    [feat] seed default user

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,20 @@ import { Logger } from './utils/Logger';
 import { app } from './app';
 import sequelize from './DAL/database';
 import config from './config/config';
+import User from './DAL/models/User';
+import { mockUser } from './tests/mocks';
 
 try {
-  sequelize.sync({ force: true });
+  sequelize.sync({ force: true }).then(() => {
+    User.findOne({ where: { username: mockUser.username } }).then(user => {
+      if (!user) {
+        User.create(mockUser).then(() => {
+          Logger.log('Default user created');
+        });
+      }
+    });
+  });
+
   app.listen(config.server.port, () => {
     Logger.log(`Server is running at https://localhost:${config.server.port}`);
   });
