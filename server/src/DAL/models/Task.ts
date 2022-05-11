6d248f183f2fe62bcaeb8b6519c7cd407db70c0d
András Balogh <kokx_007@msn.com>
2022-05-11 13:00:04 +0200

    [feat] Task entity

---
+++ server/src/DAL/models/Task.ts
@@ -0,0 +1,86 @@
+import { Model, DataTypes, Optional } from 'sequelize';
+import sequelize from '../database';
+
+export enum TaskStatus {
+  Todo = 'Todo',
+  InProgress = 'InProgress',
+  Blocked = 'Blocked',
+  Done = 'Done',
+}
+
+interface TaskAttributes {
+  id: number;
+  title: string;
+  description: string;
+  order: number;
+  status: TaskStatus;
+  tenantId?: number;
+  parentId?: number;
+  assigneeId?: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+}
+
+export interface TaskInput extends Optional<TaskAttributes, 'id' | 'tenantId' | 'parentId' | 'assigneeId'> {}
+export interface TaskOutput extends Required<TaskAttributes> {}
+
+class Task extends Model<TaskAttributes, TaskInput> implements TaskAttributes {
+  public id!: number;
+  public title!: string;
+  public description!: string;
+  public order!: number;
+  public status!: TaskStatus;
+  public tenantId!: number;
+  public parentId!: number;
+  public assigneeId!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  public readonly deletedAt!: Date;
+}
+
+Task.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING(200),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    order: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM,
+      allowNull: false,
+    },
+    tenantId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    parentId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    assigneeId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: true,
+    sequelize,
+    paranoid: true,
+  }
+);
+
+export default Task;
--- server/src/DAL/models/associations.ts
+++ server/src/DAL/models/associations.ts
@@ -1,7 +1,23 @@
+import Task from './Task';
 import Tenant from './Tenant';
 import User from './User';
 
-Tenant.hasOne(User, {
+Tenant.hasMany(User, {
   foreignKey: 'tenantId',
 });
 User.belongsTo(Tenant);
+
+Tenant.hasMany(Task, {
+  foreignKey: 'tenantId',
+});
+Task.belongsTo(Tenant);
+
+User.hasMany(Task, {
+  foreignKey: 'assigneeId',
+});
+Task.belongsTo(User);
+
+Task.hasMany(Task, {
+  foreignKey: 'parentId',
+});
+Task.belongsTo(Task);
