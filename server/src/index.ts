0d49e496a6b73df8192c395ef7e7462427e23b79
András Balogh <kokx_007@msn.com>
2022-05-12 09:17:09 +0200

    [fix] default user

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,15 +4,18 @@ import sequelize from './DAL/database';
 import config from './config/config';
 import User from './DAL/models/User';
 import { mockUser } from './tests/mocks';
+import SecurityHelper from './utils/SecurityHelper';
 
 try {
   sequelize.sync({ force: true }).then(() => {
-    User.findOne({ where: { username: mockUser.username } }).then(user => {
-      if (!user) {
-        User.create(mockUser).then(() => {
-          Logger.log('Default user created');
-        });
-      }
+    SecurityHelper.hashPassword(mockUser.password).then(password => {
+      User.findOne({ where: { username: mockUser.username } }).then(user => {
+        if (!user) {
+          User.create({ ...mockUser, password }).then(() => {
+            Logger.log('Default user created');
+          });
+        }
+      });
     });
   });
 
