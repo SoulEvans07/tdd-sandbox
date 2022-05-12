d68e2c769595b44235514ad0e1b0593ecb4f215f
András Balogh <kokx_007@msn.com>
2022-05-12 09:17:24 +0200

    [fix] logger middleware

--- server/src/middleware/ApiEndpointLoggerMiddleware.ts
+++ server/src/middleware/ApiEndpointLoggerMiddleware.ts
@@ -4,7 +4,11 @@ import { Logger } from '../utils/Logger';
 
 export default class ApiEndpointLoggerMiddleware {
   public static log(req: AuthorizedRequest, _: unknown, next: NextFunction) {
-    Logger.log(`Handle request: ${req.method.toLowerCase()}, ${req.url}, ${req.body}, ${req.query}, ${req.user}`);
+    Logger.log(
+      `Handle request: ${req.method.toLowerCase()}, url: ${req.url}, body: ${
+        req.body ? JSON.stringify(req.body) : '-'
+      }, query: ${req.query ? JSON.stringify(req.query) : '-'}, user: ${req.user ? JSON.stringify(req.user) : '-'}`
+    );
     return next();
   }
 }
