80b75bc4e02c77a692b5db7323ed0f36d7edc56d
Adam Szi <szi.adam@simonyi.bme.hu>
2022-06-29 17:19:41 +0200

    fixed artifact paths
    ping

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -11,8 +11,6 @@ const config: Partial<Cypress.UserConfigOptions<any>> = {
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./test/plugins/index.ts').default(on, config);
     },
